a8850bf129190412245647b3d56f18e02de3a092
gaso <gustavo2840@gmail.com>
2018-09-25 14:30:39 -0500

    agregando indices a columnas

--- models/medicion.py
+++ models/medicion.py
@@ -4,8 +4,8 @@ from openerp import models,fields
 class Medicion(models.Model):
 	_name='uniquindio.medicion'
 
-	estacion_id = fields.Many2one(string='Estacion',required=True,comodel_name='uniquindio.estacion')
-	tipo_id = fields.Many2one(string='Tipo',required=True,comodel_name='uniquindio.tiposensor')	
-	valor = fields.Float('Valor', required=True , group_operator="avg")
-	unidad = fields.Char('Unidad Medida', required=True)
+	estacion_id = fields.Many2one(string='Estacion',required=True,comodel_name='uniquindio.estacion',index=True)
+	tipo_id = fields.Many2one(string='Tipo',required=True,comodel_name='uniquindio.tiposensor',index=True)	
+	valor = fields.Float('Valor', required=True , group_operator="avg",index=True)
+	unidad = fields.Char('Unidad Medida', required=True,index=True)
 
