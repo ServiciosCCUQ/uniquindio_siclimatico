f25c03c1cbb8ca638b3f75c41d267573f4175331
gaso <gustavo2840@gmail.com>
2018-09-25 08:42:38 -0500

    refactorizar nombre de campo

--- controller/__init__.py
+++ controller/__init__.py
@@ -18,5 +18,4 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from models import *
-from controller import *
+import controller
