6ec6961ae0679c1de3c07039f5765a3a65705dbf
gaso <gustavo2840@gmail.com>
2019-02-03 21:28:05 -0500

    Prueba de tiposensor

--- uniquindio_siclimatico/tests/test_tiposensor.py
+++ uniquindio_siclimatico/tests/test_tiposensor.py
@@ -40,7 +40,7 @@ class TestTipoSensor(TransactionCase):
         self.csp_admin = self.env.ref('uniquindio_siclimatico.csp_admin')
         self.csp_inve = self.env.ref('uniquindio_siclimatico.csp_investigador')
         self.csp_estacion = self.env.ref('uniquindio_siclimatico.csp_estacion')
-        
+
         # groups_data = self.res_users.read_group(cr, uid, domain,
 
         # No enviar confirmacion para el reinicio de clave
